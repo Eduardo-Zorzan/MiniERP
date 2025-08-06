e37527dba723930e61876e2c4afc642245781722
Eduardo Zorzan <duduzorzan@gmail.com>
2025-08-05 22:14:45 -0300

    init API development

---
+++ API/Database/Context.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Reflection.Metadata;
+using DotNetEnv;
+
+namespace API.Database
+{
+	public class Context : DbContext
+	{
+		public DbSet<Models.Users> Users => Set<Models.Users>();
+
+		protected override void OnConfiguring(DbContextOptionsBuilder options)
+		{
+			Env.Load();
+
+			string ? connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION")
+			?? throw new InvalidOperationException("Database connection string is not set.");
+			options.UseSqlServer(connectionString);
+		}
+	}
+}
